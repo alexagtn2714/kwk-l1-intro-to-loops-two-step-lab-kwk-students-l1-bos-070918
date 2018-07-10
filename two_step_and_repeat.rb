a9359cbe6b3e5e68cb84c0cba34a019108f65fb8
alexagtn2714 <alexagaton@hotmail.com>
2018-07-10 20:26:37 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -4,6 +4,8 @@ loop do
   puts "step to the right."
 break  
 end  
+
+ puts "now jump up and down!"
   # Write a loop that outputs "Right foot back", "Left foot back",
   # "Right foot back", and "Stop", with sleep after each
 end
